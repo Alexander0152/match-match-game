8adff8dedf93c37d5eac29b0b85ef1234b87bb0c
Alexander0152 <66972872+Alexander0152@users.noreply.github.com>
2021-05-23 23:57:05 +0300

    add: db listeners

--- src/app/dataAccessLayer/userDao.ts
+++ src/app/dataAccessLayer/userDao.ts
@@ -11,21 +11,48 @@ export default class UserDao {
   }
 
   public addUser(user: User): void {
-    let openRequest = indexedDB.open(this.dbName, this.dbVersion);
+    const openRequest = indexedDB.open(this.dbName, this.dbVersion);
 
-    let db;
-    const dbReq = indexedDB.open('myDB', 1);
-    dbReq.onupgradeneeded = (event) => {
-      // Зададим переменной db ссылку на базу данных
-      db = event.target.result;
-      // Создадим хранилище объектов с именем notes.
-      let notes = db.createObjectStore('notes', { autoIncrement: true });
+    // prefixes of implementation that we want to test
+    indexedDB = window.indexedDB;
+
+    // prefixes of window.IDB objects
+    // window.IDBTransaction =
+    //   window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+    // window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+    if (!window.indexedDB) {
+      window.alert("Your browser doesn't support a stable version of IndexedDB.");
+    }
+
+    openRequest.onerror = function (event) {
+      console.log('error: ');
     };
-    dbReq.onsuccess = (event) => {
-      db = event.target.result;
-    };
-    dbReq.onerror = (event) => {
-      alert('error opening database ' + event.target.errorCode);
-    };
+
+    openRequest.addEventListener('error');
   }
 }
+// public addUser(user: User): void {
+//     let openRequest = indexedDB.open(this.dbName, this.dbVersion);
+
+//     let db;
+//     const dbReq = indexedDB.open('myDB', 1);
+
+//     dbReq.onupgradeneeded = (event) => {
+//       // Зададим переменной db ссылку на базу данных
+//       db = event.target.result;
+//       // Создадим хранилище объектов с именем notes.
+//       let users = db.createObjectStore('users', { autoIncrement: true });
+//     };
+//     dbReq.onsuccess = (event) => {
+//       db = event.target.result;
+//     };
+//     dbReq.onerror = (event) => {
+//       alert('error opening database ' + event.target.errorCode);
+//     };
+
+//     dbReq.onsuccess = (event) => {
+//       db = event.target.result;
+//       addStickyNote(db, 'Hello world first time!');
+//     };
+//   }
--- src/serviceLayer/userService.ts
+++ src/serviceLayer/userService.ts
@@ -2,8 +2,8 @@ import UserDao from '../app/dataAccessLayer/userDao';
 import User from '../businessLayer/user';
 
 export default class UserService {
-  public static addUser(dbName: string, user: User): void {
-    const userDao: UserDao = new UserDao(dbName);
+  public static addUser(dbName: string, dbVersion: number, user: User): void {
+    const userDao: UserDao = new UserDao(dbName, dbVersion);
     userDao.addUser(user);
   }
 }
